198f9f67aae0e2e1336b31ddae05653dfb962ce6
kikemc557 <kikemeca05@gmai.com>
2021-03-08 17:43:09 -0600

    Archivo PDF con ejercicios de la prueba

---
+++ MemoriaDinamica.cpp
@@ -0,0 +1,4 @@
+/*Diseña, codifica y comenta el código de una función en C++ que sea
+capaz de asignar dinámicamente la memoria a un conjunto de datos de
+tipo array que almacene nombres de usuario, reservando y liberando
+la memoria para un conjunto de 10 registros.*/
